32444927ac0bcd4599fc285d2022528bdbf856d0
José María Valera Reales <chemaclass@outlook.es>
2014-05-07 01:39:18 +0200

    rellenamos el tablero con células muertas en el Fill()

--- src/main.go
+++ src/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-const TAMANO_TABLERO = 10
+//Tamaño del tablero
+const TAMANO_TABLERO = 15
+const TIEMPO_ACTUALIZACION = time.Second
 
-type Tablero [][]int
+//Estados de una célula
+const VIVA = "*"
+const MUERTA = " "
+
+type Celula string
+type Tablero [][]Celula
 
 type Juego struct {
 	Tablero
@@ -20,17 +28,41 @@ func (j Juego) String() string {
 	return str
 }
 
-func (j *Juego) Fill(cant int) {
-	j.Tablero = make([][]int, cant)
-	for i, _ := range j.Tablero {
-		j.Tablero[i] = make([]int, cant)
+/* Rellenar todo el tablero */
+func (self *Juego) Fill() {
+	//Inicializamos el tablero
+	self.Tablero = make([][]Celula, TAMANO_TABLERO)
+	for i, _ := range self.Tablero {
+		self.Tablero[i] = make([]Celula, TAMANO_TABLERO)
+	}
+	//rellenamos con células muertas
+	for i, v := range self.Tablero {
+		for j, _ := range v {
+			self.Tablero[i][j] = MUERTA
+		}
 	}
 }
 
+
 func main() {
-	fmt.Println("> main()")
 	j := new(Juego)
-	j.Fill(TAMANO_TABLERO)
-	fmt.Println("Fill:", j)
-	fmt.Println("< main()")
+	j.Fill()
+	j.fillTest()
+	for {
+		fmt.Println(j)
+		
+		time.Sleep(TIEMPO_ACTUALIZACION)
+	}
+}
+
+/* Rellenar todo el tablero */
+func (j *Juego) fillTest() {
+	j.Tablero[0][0] = VIVA
+	j.Tablero[0][1] = VIVA
+	j.Tablero[1][0] = VIVA
+	j.Tablero[1][2] = VIVA
+	j.Tablero[2][1] = VIVA
+	j.Tablero[3][5] = VIVA
+	j.Tablero[2][2] = VIVA
+	j.Tablero[3][4] = VIVA
 }
